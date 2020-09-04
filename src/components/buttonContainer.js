601fb2d57830a61ee50295c7445154452e38dfb0
Alex Chui <ahhchui@gmail.com>
2020-09-04 07:25:10 -0700

    added intial batch of logic

--- src/components/buttonContainer.js
+++ src/components/buttonContainer.js
@@ -4,11 +4,53 @@ import "./stylesheets/buttonContainer.css";
 import Button from "./button.js";
 
 
-function ButtonContainer() {
+function ButtonContainer({gameRunning, sequence}) {
+
+  const [playingSequence, setPlayingSequence] = React.useState(false);
+  const [playerTurn, setPlayerTurn] = React.useState(false);
+  const [playerIndex, setPlayerIndex] = React.useState(0);
+
+  const colors = ["green", "red", "yellow", "blue"]
+
+  React.useEffect(() => {
+    if (gameRunning && !playerTurn) {
+      playSequence();
+    }
+    if (playerTurn) {
+      console.log("its ur turn bb")
+    }
+  },[gameRunning, playerTurn, playerIndex])
+
+  const playSequence = () => {
+    setPlayingSequence(true);
+
+    let i = 0;
+    let interval = setInterval(() => {
+      if (i === sequence.length - 1) {
+        setPlayingSequence(false); 
+        setPlayerTurn(true);
+        clearInterval(interval);
+      }
+      console.log(sequence[i]);
+      i++;
+    }, 700)
+  }
+
+  const clickButton = (e) => {
+    console.log(e.target);
+  }
 
   return (
     <div className="buttonContainer">
-
+      {
+        colors.map((color) => 
+          <Button 
+            key={color}
+            color={color} 
+            onClick={(e) => clickButton(e)}
+          />
+        )
+      }
     </div>
   )
 }
--- src/components/game.js
+++ src/components/game.js
@@ -6,9 +6,59 @@ import ButtonContainer from "./buttonContainer.js";
 
 function Game() {
 
+  const [level, setLevel] = React.useState(1);
+  const [score, setScore] = React.useState(0);
+  const [sequence, setSequence] = React.useState([]);
+  const [gameRunning, setGameRunning] = React.useState(false);
+  const [gameOver, setGameOver] = React.useState(false);
+
+  const colors = ["green", "red", "yellow", "blue"]
+
+  const createSequence = (level) => {
+    const seq = [];
+    for (let i = 0; i < level; i++) {
+      const randIdx = Math.floor(Math.random() * 4);
+      seq.push(colors[randIdx]); 
+    }
+    setSequence(seq);
+  }
+
+  const restart = () => {
+    setLevel(1);
+    setScore(0);
+    setSequence([]);
+  }
+  
+  const getIsRunning = () => {
+    return gameRunning;
+  }
+
+  const getSequence = () => {
+    return sequence;
+  }
+
+  const start = () => {
+    setGameRunning(true);
+
+    createSequence(level);
+  }
+
   return (
     <div className="game">
-
+      <div className="display">
+        {
+          (gameOver) ?
+            <div className="restartButton" onClick={restart}>restart</div>
+            : (gameRunning) ? 
+            <div className="score">{score}</div>
+            : <div className="startButton" onClick={start}>start</div>
+        }
+      </div>
+      
+      <ButtonContainer 
+        gameRunning={gameRunning}
+        sequence={sequence}
+      />
     </div>
   )
 }
