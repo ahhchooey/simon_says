ad0a8341d5eb90e4a5a325d14058f6bc2cc4bebc
Alex Chui <ahhchui@gmail.com>
2020-09-04 11:39:15 -0700

    Updates

---
+++ precache-manifest.b155ce12b2b17483599123cddca1edc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e33876b8dd2c7683b32076f0f581ef5",
+    "url": "/simon_says/index.html"
+  },
+  {
+    "revision": "6fff48cb615d5b9f13ac",
+    "url": "/simon_says/static/css/main.858ea999.chunk.css"
+  },
+  {
+    "revision": "32974c6d85095aa2fe40",
+    "url": "/simon_says/static/js/2.7219f510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simon_says/static/js/2.7219f510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fff48cb615d5b9f13ac",
+    "url": "/simon_says/static/js/main.58411c82.chunk.js"
+  },
+  {
+    "revision": "86fb5ef03d0ec298eeff",
+    "url": "/simon_says/static/js/runtime-main.b9b1e1a4.js"
+  }
+]);
